4209e9be97dfc832c8f6d4ac5fa2ae2119aa3d87
tde-desc <dennis.schneider02@gmail.com>
2021-02-18 17:57:19 +0100

    Add htmlunit

--- src/main/java/de/dsms/rsscreator/infrastructure/http/HttpConnectionWrapper.java
+++ src/main/java/de/dsms/rsscreator/infrastructure/http/HttpConnectionWrapper.java
@@ -1,9 +1,9 @@
 package de.dsms.rsscreator.infrastructure.http;
 
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import de.dsms.rsscreator.application.feed.service.HttpRequester;
 import lombok.extern.slf4j.Slf4j;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -14,9 +14,12 @@ import java.util.Optional;
 public class HttpConnectionWrapper implements HttpRequester {
     @Override
     public Optional<String> request(String urlString) {
+        WebClient client = new WebClient();
+        client.getOptions().setCssEnabled(true);
+        client.getOptions().setJavaScriptEnabled(false);
         try {
-            Document doc = Jsoup.connect(urlString).get();
-            return Optional.of(doc.outerHtml());
+            HtmlPage page = client.getPage(urlString);
+            return Optional.of(page.getWebResponse().getContentAsString());
         } catch (IOException e) {
             log.error("URL could not be evaluated", e);
         }
