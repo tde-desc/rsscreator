05cfee36672738666f41596b99598e034acc101c
tde-desc <dennis.schneider02@gmail.com>
2021-02-18 16:50:59 +0100

    Add jsoup

--- src/main/java/de/dsms/rsscreator/infrastructure/http/HttpConnectionWrapper.java
+++ src/main/java/de/dsms/rsscreator/infrastructure/http/HttpConnectionWrapper.java
@@ -2,14 +2,11 @@ package de.dsms.rsscreator.infrastructure.http;
 
 import de.dsms.rsscreator.application.feed.service.HttpRequester;
 import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Component;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.Optional;
 
 @Slf4j
@@ -18,22 +15,10 @@ public class HttpConnectionWrapper implements HttpRequester {
     @Override
     public Optional<String> request(String urlString) {
         try {
-            final URL url = new URL(urlString);
-            final HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setInstanceFollowRedirects(true);
-            try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-                String inputLine;
-                StringBuilder content = new StringBuilder();
-                while ((inputLine = in.readLine()) != null) {
-                    content.append(inputLine);
-                }
-                return Optional.of(content.toString());
-            }
-        } catch (MalformedURLException e) {
-            log.error("URL is malformed", e);
+            Document doc = Jsoup.connect(urlString).get();
+            return Optional.of(doc.outerHtml());
         } catch (IOException e) {
-            log.error("Error performing request", e);
+            log.error("URL could not be evaluated", e);
         }
         return Optional.empty();
     }
