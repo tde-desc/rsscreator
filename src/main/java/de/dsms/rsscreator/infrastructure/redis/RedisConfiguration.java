9009e067c1701c9c1ae48cb7b18a1ea007d64694
tde-desc <dennis.schneider02@gmail.com>
2021-04-04 15:37:22 +0200

    Stackhero redis

--- src/main/java/de/dsms/rsscreator/infrastructure/redis/RedisConfiguration.java
+++ src/main/java/de/dsms/rsscreator/infrastructure/redis/RedisConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import redis.clients.jedis.Protocol;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -16,15 +15,11 @@ public class RedisConfiguration {
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() throws URISyntaxException {
         JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
-        String redisUrl = System.getenv("REDISTOGO_URL");
+        String redisUrl = System.getenv("STACKHERO_REDIS_URL_TLS");
         if (StringUtils.isNotBlank(redisUrl)) {
             URI redisURI = new URI(redisUrl);
-            String pw = redisURI.getUserInfo().split(":", 2)[1];
             RedisStandaloneConfiguration config = jedisConnectionFactory.getStandaloneConfiguration();
-            config.setPort(redisURI.getPort());
-            config.setPassword(pw);
             config.setHostName(redisURI.getHost());
-            jedisConnectionFactory.setTimeout(Protocol.DEFAULT_TIMEOUT);
         }
         return jedisConnectionFactory;
     }
